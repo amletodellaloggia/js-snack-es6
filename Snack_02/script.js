4bf06671ea2eb8768699a93ace7867e576d2c5f9
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-14 15:11:07 +0200

    [upd] for cycle, new array, new temp array, updated properties, console.log

--- Snack_02/script.js
+++ Snack_02/script.js
@@ -18,3 +18,34 @@ const footballTeams = [
 		receivedFouls: 0
 	}
 ];
+
+// Ho bisogno di aggiornare i valori 'score' e 'receivedFouls' con un numero randomico
+
+// Creo un nuovo array dove inserisco degli oggetti con i valori già definiti e creo una variabile che conterrà gli oggetti con i valori aggiornati
+const footballTeamsUpdated = [];
+
+// Ciclo l'array, dichiaro una variabile per contenere i valori aggiornati, mantenendo il valore 'teamName' come lo stesso, mi servo di math.floor e math.random per score (da 0 a 90) e per receivedFouls (da 0 a 5) e pusho tutto nel nuovo array footballTeamsUpdated
+for(let i=0; i<footballTeams.length; i++){
+	const updatedValues = {
+		teamName: footballTeams[i].teamName,
+		score: Math.floor(Math.random() * 91),
+		receivedFouls: Math.floor(Math.random() * 6)
+	};
+footballTeamsUpdated.push(updatedValues);
+};
+
+// Creo un array che contiene oggetti le cui proprietà riportate saranno solamente 'teamName' e 'receivedFouls'
+const footballTeamsNew = [];
+
+// Ciclo l'array aggiornato e dichiaro una variabile 'onlyTwoValues' (che contiene solo due valori richiesti e presi direttamente dai valori aggiornati) e la pusho nel nuovo array di oggetti dichiarato in precedenza
+for(let i=0; i<footballTeamsUpdated.length; i++){
+	const onlyTwoValues = {
+		teamName: footballTeamsUpdated[i].teamName, // Sto puntando all'array aggiornato, non all'originale
+		receivedFouls: footballTeamsUpdated[i].receivedFouls // Sto puntando all'array aggiornato, non all'originale
+	};
+footballTeamsNew.push(onlyTwoValues);
+};
+
+// Stampo in console
+console.log(footballTeamsNew);
+

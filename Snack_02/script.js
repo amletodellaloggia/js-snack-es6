b91a5f690736f24e74fe43a7015dad43dce0b129
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-14 14:30:56 +0200

    [upd] objects array, properites

---
+++ Snack_02/script.js
@@ -0,0 +1,20 @@
+// SVOLGIMENTO SNACK_02
+// Dichiaro un array di oggetti 'footballTeams' che contine oggetti con proprietà 'teamName' (nome, unica inizializzata), 'score' (punti fatti), 'receivedFouls' (falli subiti)
+
+const footballTeams = [
+	{
+		teamName: "Manchester United FC",
+		score: 0,
+		receivedFouls: 0
+	},
+	{
+		teamName: "Barcelona FC",
+		score: 0,
+		receivedFouls: 0
+	},
+	{
+		teamName: "Bayern Munich FC",
+		score: 0,
+		receivedFouls: 0
+	}
+];
